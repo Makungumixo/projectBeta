a0b539f0bf3f8048981c0fefef54bb6a31f54370
Makungumixo <93745143+Makungumixo@users.noreply.github.com>
2025-03-16 12:57:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
   const darkModeToggle = document.getElementById('dark-mode-toggle');
 
   if (localStorage.getItem('darkMode') === 'enabled') {
     document.body.classList.add('dark-mode');
   }
 
-  darkModeToggle.addEventListener('click', function(e) {
-    e.preventDefault(); // prevents any default behavior
+  darkModeToggle.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     localStorage.setItem('darkMode', 
       document.body.classList.contains('dark-mode') ? 'enabled' : 'disabled'
     );
   });
 });
-
 
