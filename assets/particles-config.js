3cbcf0d6dd09488feb5d893a0dd3fabb2c99db6c
Makungumixo <93745143+Makungumixo@users.noreply.github.com>
2025-03-28 17:16:47 +0200

    Update particles-config.js

--- assets/particles-config.js
+++ assets/particles-config.js
@@ -1,10 +1,10 @@
 particlesJS('particles-js', {
   "particles": {
     "number": {
-      "value": 100,
+      "value": 80,
       "density": {
         "enable": true,
-        "value_area": 1200
+        "value_area": 800
       }
     },
     "shape": {
