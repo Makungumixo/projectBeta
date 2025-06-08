34edc1cc8559ac4872e8124d1530616431bc93c7
Makungumixo <93745143+Makungumixo@users.noreply.github.com>
2025-06-08 11:38:25 +0200

    Update particles-config.js

--- assets/particles-config.js
+++ assets/particles-config.js
@@ -1,66 +1,66 @@
-tsParticles.load("tsparticles", {
-  particles: {
-    number: {
-      value: 120,
-      density: {
-        enable: true,
-        value_area: 1200
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 1200
       }
     },
-    shape: {
-      type: "triangle"
+    "shape": {
+      "type": "triangle"
     },
-    color: {
-      value: "#ffffff"
+    "color": {
+      "value": "#ffffff"
     },
-    opacity: {
-      value: 0.5,
-      random: true
+    "opacity": {
+      "value": 0.5,
+      "random": true
     },
-    size: {
-      value: 5,
-      random: true
+    "size": {
+      "value": 5,
+      "random": true
     },
-    move: {
-      enable: true,
-      speed: 3,
-      direction: "none",
-      random: true,
-      straight: false,
-      out_mode: "out",
-      bounce: false
+    "move": {
+      "enable": true,
+      "speed": 4,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
     },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#ffffff",
-      opacity: 0.4,
-      width: 1
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
     }
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "grab"
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
       },
-      onclick: {
-        enable: true,
-        mode: "push"
+      "onclick": {
+        "enable": true,
+        "mode": "push"
       }
     },
-    modes: {
-      grab: {
-        distance: 200,
-        line_linked: {
-          opacity: 1
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {
+          "opacity": 1
         }
       },
-      push: {
-        particles_nb: 4
+      "push": {
+        "particles_nb": 4
       }
     }
   },
-  retina_detect: true
+  "retina_detect": true
 });
